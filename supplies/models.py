1b2fa5ca90c42425c3b580eafc1377bc3b3771d6
Forte <smartbabs02@gmail.com>
2024-10-01 16:35:16 +0100

    refactored the ItemCategory model to display item category in Item model

--- supplies/models.py
+++ supplies/models.py
@@ -3,10 +3,26 @@ from django.utils import timezone
 from django.contrib import admin
 from django.contrib.auth.models import User
 
+
+def category_name_default():
+    return {"category_name_default": "General"}
+
+class ItemCategory(models.Model):
+    cat_name = models.CharField(max_length=70)
+    cat_description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.cat_name
+
+
 # Item model
 class Item(models.Model):
     item_name = models.CharField(max_length=200)
     description = models.TextField()
+    item_category = models.ForeignKey(ItemCategory, on_delete=models.CASCADE, default=None, null=True, blank=True)
     quantity = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -20,16 +36,3 @@ class Item(models.Model):
     #     ordering='created_by',
     #     description='Items added by',
     # )
-
-
-class ItemCategory(models.Model):
-    cat_name = models.CharField(max_length=70)
-    cat_description = models.TextField()
-    cat_type = models. ForeignKey(Item, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    last_updated = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.cat_name
-
