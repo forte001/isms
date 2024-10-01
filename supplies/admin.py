1c2dcc79cda0d6b0e30887f7013bba3f270cbea0
Forte <smartbabs02@gmail.com>
2024-10-01 14:33:39 +0100

    created and configured ItemCategory model

--- supplies/admin.py
+++ supplies/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Item
+from .models import Item, ItemCategory
 
 # Register your models here.
-admin.site.register(Item)
+# admin.site.register(Item)
+# admin.site.register(ItemCategory)
+admin.site.register([Item, ItemCategory])
