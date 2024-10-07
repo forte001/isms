f5feec65776334c00ad403e7e7919650d35a3af9
Forte <smartbabs02@gmail.com>
2024-10-07 13:20:50 +0100

    refactored Test Models

--- supplies/tests.py
+++ supplies/tests.py
@@ -13,55 +13,52 @@ class BaseTestCase(TestCase):
     """
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='testpass')
-
+        self.category = Category.objects.create(cat_name='Electronics', created_by=self.user)
+        self.supplier = Supplier.objects.create(supplier_name='Supplier A', supplier_email='contact@supplier.com', supplier_phone='+2347086497348')
+        self.customer = Customer.objects.create(customer_first_name='John', customer_last_name='Doe', customer_email='johndoe@company.com', customer_phone='+233807487487')
+        self.product = Product.objects.create(product_name='Test Product', description='some product', stock_quantity='4', category=self.category, supplier=self.supplier, price=100.0)
+        self.sale = Sale.objects.create(product=self.product, customer=self.customer, quantity=2, total_price='200.00')
+        self.adjustment = StockAdjustment.objects.create(product=self.product, quantity_adjusted=5, reason='quantity low')
 ### Model Tests 
 class CategoryModelTest(BaseTestCase):
     def setUp(self):
-    # Create test user
+    # References the BaseTest class and made available for extension and use in other Test classes.
         super().setUp() 
     def test_str(self):
-        category = Category.objects.create(cat_name='Electronics', created_by=self.user)
-        self.assertEqual(str(category), 'Electronics')
+        
+        self.assertEqual(str(self.category), 'Electronics')
 
-class SupplierModelTest(TestCase):
-    def test_str(self):
-        supplier = Supplier.objects.create(supplier_name='Supplier A', supplier_email='contact@supplier.com', supplier_phone='+2347086497348')
-        self.assertEqual(str(supplier), 'Supplier A')
+class SupplierModelTest(BaseTestCase):
+    def setUp(self):
+        super().setUp()
 
-class CustomerModelTest(TestCase):
     def test_str(self):
-        customer = Customer.objects.create(customer_first_name='John', customer_last_name='Doe', customer_email='johndoe@company.com', customer_phone='+233807487487')
-        self.assertEqual(str(customer), 'John'+ ' Doe')
+        self.assertEqual(str(self.supplier), 'Supplier A')
+
+class CustomerModelTest(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+
+    def test_str(self):
+        self.assertEqual(str(self.customer), 'John Doe')
 
 class ProductModelTest(BaseTestCase):
     def setUp(self):
         super().setUp()
-        self.category = Category.objects.create(cat_name='Electronics', created_by=self.user)
-        self.supplier = Supplier.objects.create(supplier_name='Supplier A', supplier_email='contact@supplier.com', supplier_phone='+2347086497348')
-        self.product = Product.objects.create(product_name='Test Product', description='some product', stock_quantity='4', category=self.category, supplier=self.supplier, price=100.0)
-
+        
     def test_str(self):
         self.assertEqual(str(self.product), 'Test Product')
 
 class SaleModelTest(BaseTestCase):
     def setUp(self):
         super().setUp()
-        self.category = Category.objects.create(cat_name='Electronics', created_by=self.user)
-        self.supplier = Supplier.objects.create(supplier_name='Supplier A', supplier_email='contact@supplier.com', supplier_phone='+2347086497348')
-        self.product = Product.objects.create(product_name='Test Product', category=self.category, supplier=self.supplier, price=100.0)
-        self.customer = Customer.objects.create(customer_first_name='John', customer_last_name='Doe', customer_email='johndoe@company.com', customer_phone='+233807487487')
-        self.sale = Sale.objects.create(product=self.product, customer=self.customer, quantity=2, total_price='200.00')
-
+   
     def test_str(self):
         self.assertEqual(str(self.sale), f'Sale of {self.sale.quantity} x {self.sale.product.product_name} to {self.customer.customer_first_name} {self.customer.customer_last_name}')
 
 class StockAdjustmentModelTest(BaseTestCase):
     def setUp(self):
         super().setUp()
-        self.category = Category.objects.create(cat_name='Electronics', created_by=self.user)
-        self.supplier = Supplier.objects.create(supplier_name='Supplier A', supplier_email='contact@supplier.com', supplier_phone='+2347086497348')
-        self.product = Product.objects.create(product_name='Test Product', category=self.category, supplier=self.supplier, price=100.0)
-        self.adjustment = StockAdjustment.objects.create(product=self.product, quantity_adjusted=5, reason='quantity low')
 
     def test_str(self):
         self.assertEqual(str(self.adjustment), f'{self.adjustment.quantity_adjusted} of {self.adjustment.product.product_name} - {self.adjustment.reason} on {self.adjustment.date}')
